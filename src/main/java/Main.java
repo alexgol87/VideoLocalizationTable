1fc52610aeeaee24ade117319cd0324e47150adc
Aleksey Golovachev <alexey.golovachev@gmail.com>
2021-09-08 02:20:36 +0700

    Add comments in Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,6 @@ import util.GoogleDriveCommunitySpider;
 import util.GoogleDriveSpider;
 
 import java.time.Instant;
-import java.util.Arrays;
 
 import static util.GeneralUtil.*;
 import static util.GoogleDriveSpider.videoErrorsCE;
@@ -13,16 +12,23 @@ import static util.GoogleDriveSpider.videoErrorsCE;
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
+        // забираем время последнего обновления COEM Videos и выводим его
         String lastUpdateTime = GoogleDriveApiUtil.getModifiedTime(GoogleDriveApiUtil.buildSheetsApiClientService(), "1SC92tKYXQDqujUcvZVYMmmNiJp35Q1b22fKg2C7zeQI", "video COEm!R1:R1");
         System.out.println("Last update time: " + lastUpdateTime);
+        // начинаем фиксировать время старта работы скрипта
         Instant start = startTimeFixing();
+        // запускаем задание на парсинг videos / banners / community banners, раскомментируем нужную строку.
         Runnable task = GoogleDriveCommunitySpider::new;
       //  Runnable task = GoogleDriveSpider::new;
       //  Runnable task = GoogleDriveBannerSpider::new;
+        // стартуем поток
         Thread thread = new Thread(task);
         thread.start();
+        // ожидаем выполнение потока
         thread.join();
+        // выводим время работы скрипта
         System.out.println(endTimeFixing(start));
+        // выводим ошибки парсинга COEM videos
         for (String s : videoErrorsCE) {
             System.out.println(s);
         }
